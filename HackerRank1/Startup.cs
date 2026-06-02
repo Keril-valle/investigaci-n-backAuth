2e0b695c19af7a9d39c45fca597d10dcd9e15f97
Keril-valle <kvalle1306@gmail.com>
2026-06-01 18:43:33 -0600

    Allow local frontend origin in CORS

--- HackerRank1/Startup.cs
+++ HackerRank1/Startup.cs
@@ -67,7 +67,11 @@ namespace LibraryService.WebAPI
             {
                 options.AddPolicy("Devcors", policy =>
                 {
-                    policy.WithOrigins("https://investigacin-front-keril.netlify.app")
+                    policy.WithOrigins(
+                                "https://investigacin-front-keril.netlify.app",
+                                "http://localhost:5173",
+                                "https://localhost:5173",
+                                "http://127.0.0.1:5173")
                           .AllowAnyMethod()
                           .AllowAnyHeader()
                           .AllowCredentials();
